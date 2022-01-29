9b295ddf9d789e10c20565927210a0f23bb6e72b
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2022-01-28 21:29:14 -0600

    sample solution added

---
+++ assignments/org-mode/solutions/first.c
@@ -0,0 +1,22 @@
+/* My first Org-mode file */
+
+/*   This code block This Org-mode file prints ~hello world~ and creates */
+/*   a source file ~first.c~ that can be compiled and executed on the */
+/*   command line. */
+
+/*   #+name: hello_world_program */
+
+/* [[file:MarcusBirkenkrahe.org::hello_world_program][hello_world_program]] */
+#include "stdio.h"
+
+
+
+
+
+
+int main() {
+// print a string
+puts("hello world");
+return 0;
+}
+/* hello_world_program ends here */
