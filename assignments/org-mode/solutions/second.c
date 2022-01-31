75fddb414eefb97aa5b2425daffa61a6305e741d
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2022-01-31 09:32:34 -0600

    updates

---
+++ assignments/org-mode/solutions/second.c
@@ -0,0 +1,18 @@
+/* My second C code block */
+
+/*    The following code block prints "hello world" using the C function */
+/*    printf. */
+
+/*    #+name: printf */
+
+#include "stdio.h"
+
+
+
+
+
+
+int main() {
+printf("hello world\n");
+return 0;
+}
