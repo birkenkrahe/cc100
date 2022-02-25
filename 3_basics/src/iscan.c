b3d64d067e4a1c1aaf95a12dbecafe58512c4ecf
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2022-02-25 15:06:54 -0600

    updates

--- 3_basics/src/iscan.c
+++ 3_basics/src/iscan.c
@@ -1,11 +1,4 @@
 /* Reading input */
-
-/*   * Before you can print output with ~printf~, you need to tell the */
-/*     computer, which format it should prepare for. */
-
-/*   * Just like ~printf~, the input function ~scanf~ needs to know what */
-/*     format the input data will come in. */
-
 /*   * The following statement reads an ~int~ value and stores it in the */
 /*     variable ~i~. To run it, you have to tangle the code block (with */
 /*     ~C-u C-c C-v t~), compile and run it on the command line - not on */
@@ -15,11 +8,6 @@
 
 /* [[file:../README.org::iscan][iscan]] */
 #include <stdio.h>
-
-
-
-
-
 
 int main() {
 int i;
