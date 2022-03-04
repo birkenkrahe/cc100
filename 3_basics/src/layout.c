ef8348922e555e3b3ef3590b7c44f744ee23687c
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2022-03-04 10:51:24 -0600

    updates

---
+++ 3_basics/src/layout.c
@@ -0,0 +1,16 @@
+/* * [ ] Practice: improve the layout of this program */
+/*   then run it. */
+/*   #+name: layout */
+
+int main() {
+int var1=1;int var2;var2=
+                      var1
+                      *100;
+printf (      "Variable1=%d,variable2=%d\n",
+              var1,
+
+              var2
+              );
+return 0;
+}
+/* layout ends here */
