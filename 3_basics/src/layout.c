afe98fbac0abd5199a7ec3fc2c953bf0d7c3a99a
Marcus Birkenkrahe <birkenkrahe@outlook.com>
2022-03-04 12:16:08 -0600

    updates

--- 3_basics/src/layout.c
+++ 3_basics/src/layout.c
@@ -1,16 +1,28 @@
-/* * [ ] Practice: improve the layout of this program */
-/*   then run it. */
+
+/*   - The variable ~height~ is not declared */
+/*   - The ~printf~ function is not recognized */
+/*   - The string literal is not complete */
+
+/* * Good practice: */
+/*   - Space between tokens makes identification easier */
+/*   - Indentation makes nesting easier to spot */
+/*   - Blank lines can divide a program into logical units */
+
+/* * [ ] Practice: improve the layout of this program ([[https://drive.google.com/file/d/1FSc4gQVBf6f62qiAsJ81rGdHHkf5Tff2/view?usp=sharing][get it from */
+/*   GDrive]]), then run it. */
+
 /*   #+name: layout */
 
+/* [[file:../README.org::layout][layout]] */
 int main() {
 int var1=1;int var2;var2=
-                      var1
-                      *100;
+		      var1
+		      *100;
 printf (      "Variable1=%d,variable2=%d\n",
-              var1,
+	      var1,
 
-              var2
-              );
+	      var2
+	      );
 return 0;
 }
 /* layout ends here */
---
+++ practice/hw.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+  puts("hello world");
+}
